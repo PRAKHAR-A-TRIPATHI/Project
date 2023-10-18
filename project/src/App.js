ca8dcd3df829c3ee02fc870e5d8b73308f7e7173
Rudra-solanki <rudra.solanki@saeculumsolutions.com>
2023-10-18 16:02:55 +0530

    itemWraper

--- project/src/App.js
+++ project/src/App.js
@@ -2,15 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LogIn from './Component/Registration/LogIn';
 import Registration from './Component/Registration/Registration';
-<<<<<<< HEAD
+
 // import Home from './Component/Registration/HomeLogInBtn';
 import UserMainPage from './Component/User/UserMainPage';
 import PrivateUserRoute from './Component/PrivateRoutes/PrivateUserRoute';
 import PrivateLogInRoute from './Component/PrivateRoutes/PrivateLogInRoute';
-=======
+
 import Home from './Component/Registration/HomeLogInBtn';
 import Header from './Component/Header';
->>>>>>> dbdc43408d81abf9cf1c3113cf92a20016021222
+
 
 function App() {
   return (

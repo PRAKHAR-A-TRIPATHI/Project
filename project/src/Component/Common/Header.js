66ce2e661959f564d119abcb7cbff0e963ede484
PRAKHAR TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2023-10-18 15:54:44 +0530

    Update Header.js

--- project/src/Component/Common/Header.js
+++ project/src/Component/Common/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <div className=' text-white'>
             <header className="shadow sticky z-50 top-0">
-                <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+                <nav className=" bg-gray-400 border-gray-200 px-4 lg:px-6 py-2.5">
                     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                         <Link to="/" className="flex items-center">
                             {/* <img
@@ -28,7 +28,7 @@ function Header() {
                                 to="#"
                                 className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                             >
-                                <AiOutlineShoppingCart/>
+                                <span className='flex text-lg'><AiOutlineShoppingCart className=' text-3xl'/>cart</span>
                             </Link>
                         </div>
                         <div

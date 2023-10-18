09e05171f72a98777005a4b7d03483e963f2be80
PRAKHAR TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2023-10-18 16:13:12 +0530

    Update Header.js

--- project/src/Component/Common/Header.js
+++ project/src/Component/Common/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <div className=' text-white'>
             <header className="shadow sticky z-50 top-0">
-                <nav className=" bg-gray-400 border-gray-200 px-4 lg:px-6 py-2.5">
+                <nav className=" bg-white border-gray-200 px-4 lg:px-6 py-2.5">
                     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                         <Link to="/" className="flex items-center">
                             {/* <img
@@ -58,7 +58,7 @@ function Header() {
                                 </li>
                                 <li>
                                     <NavLink
-                                        to="/user"
+                                        to="/"
                                         className={({ isActive }) =>
                                             `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                         }

dbdc43408d81abf9cf1c3113cf92a20016021222
PRAKHAR TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2023-10-18 13:57:05 +0530

    header

--- project/src/App.js
+++ project/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import LogIn from './Component/Registration/LogIn';
 import Registration from './Component/Registration/Registration';
 import Home from './Component/Registration/HomeLogInBtn';
+import Header from './Component/Header';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/logIn" element={<LogIn />} />

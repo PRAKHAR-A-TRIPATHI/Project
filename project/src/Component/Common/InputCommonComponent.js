8b9ae162504674b640977d55b85e1b25252205e3
PRAKHAR TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2023-10-19 11:04:56 +0530

    update

--- project/src/Component/Common/InputCommonComponent.js
+++ project/src/Component/Common/InputCommonComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 const InputCommonComponent = ({ type, name, data, placeholder }) => {
     const [show, setShow] = useState(false);
